e33b8317926bc31bc4932779cd37203c3f8e59c4
Trần Đức Thịnh <95152522+thinhtranduc@users.noreply.github.com>
2021-12-28 10:24:51 +0700

    Add files via upload

---
+++ atm-simulator-v2/src/com/btmglobal/simulator/atm/customer/Person.java
@@ -0,0 +1,26 @@
+package com.btmglobal.simulator.atm.customer;
+
+public class Person {
+String name;
+    
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + '}';
+    }
+    
+}
